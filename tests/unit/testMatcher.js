71bcc201c66ba26f6f56712ccbb290ec04237769
Sergei Chipiga <Sergei.Chipiga@derivco.ee>
2018-01-09 16:31:22 +0200

    Add matcher `waitFor`

---
+++ tests/unit/testMatcher.js
@@ -0,0 +1,21 @@
+"use strict";
+
+test(".waitFor()", () => {
+    var now;
+
+    before(() => {
+        now = new Date().getTime();
+    });
+
+    chunk("passed", async () => {
+        var i = 0;
+        await expect(() => i++).to.waitFor({ "to be equal": 5 });
+        expect(new Date().getTime() - now).to.be.gte(500).and.below(1000);
+    });
+
+    chunk("failed", async () => {
+        await expect(expect(() => 0).to.waitFor({ "to be equal": 5 }))
+            .to.be.rejectedWith("expected 0 to equal 5");
+        expect(new Date().getTime() - now).to.be.gte(1000);
+    }); 
+});
