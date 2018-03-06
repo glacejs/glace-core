199adb52d6230c51a99af0f8eb790a012a754f49
Sergei Chipiga <chipiga86@gmail.com>
2018-03-06 19:18:00 +0200

    Don't check debug step in interactive mode

--- tests/e2e/testTimerSteps.js
+++ tests/e2e/testTimerSteps.js
@@ -8,15 +8,15 @@ test("It should sleep 1 sec", () => {
 
 test("It should pass timer check", () => {
     chunk(async () => {
-        SS.startTimer();
+        await SS.startTimer();
         await SS.pause(1, "sleep");
-        SS.checkTimer({ "to be above": 1 });
+        await SS.checkTimer({ "to be above": 1 });
     });
 });
 
 test("It should fail timer check", () => {
     chunk(async () => {
-        SS.startTimer();
+        await SS.startTimer();
         await SS.pause(1, "sleep");
         await SS.checkTimer({ "to be below": 1 });
     });
@@ -24,9 +24,9 @@ test("It should fail timer check", () => {
 
 test("It should fail because timer isn't started", () => {
     chunk(async () => {
-        SS.startTimer();
+        await SS.startTimer();
         await SS.pause(1, "sleep");
-        SS.stopTimer();
-        SS.checkTimer({ "to be equal": 1 });
+        await SS.stopTimer();
+        await SS.checkTimer({ "to be equal": 1 });
     });
 });
