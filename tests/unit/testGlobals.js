43c22414ee23b359a8af3b337bcbefc2627df63b
Sergei Chipiga <chipiga86@gmail.com>
2018-04-30 21:40:29 +0300

    Add stubObject helper

--- tests/unit/testGlobals.js
+++ tests/unit/testGlobals.js
@@ -174,4 +174,47 @@ scope("globals", () => {
             });
         });
     });
+
+    test(".stubObject()", () => {
+
+        chunk("stubs object", () => {
+            var x = { y: () => {}};
+            x = stubObject(x);
+            expect(x.y()).to.be.undefined;
+            expect(x.y).to.be.calledOnce;
+        });
+
+        chunk("stubs nested object", () => {
+            var x = { y: { z: () => {}}};
+            x = stubObject(x);
+            expect(x.y.z()).to.be.undefined;
+            expect(x.y.z).to.be.calledOnce;
+        });
+
+        chunk("stubs circular object", () => {
+            var x = { y: () => {}};
+            x.z = x;
+            x = stubObject(x);
+            expect(x.z.y()).to.be.undefined;
+            expect(x.z.y).to.be.calledOnce;
+        });
+
+        chunk("stubs object with return value", () => {
+            var x = { y: () => {}};
+            x = stubObject(x, 1);
+            expect(x.y()).to.be.equal(1);
+            expect(x.y).to.be.calledOnce;
+        });
+
+        chunk("stubs object with return object", () => {
+            var x = { a: () => {}, b: () => {}, c: () => {}};
+            x = stubObject(x, { a: 1, b: 2 });
+            expect(x.a()).to.be.equal(1);
+            expect(x.a).to.be.calledOnce;
+            expect(x.b()).to.be.equal(2);
+            expect(x.b).to.be.calledOnce;
+            expect(x.c()).to.be.undefined;
+            expect(x.c).to.be.calledOnce;
+        });
+    });
 });
