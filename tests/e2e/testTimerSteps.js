057e2453574bd71c14b27a75eae8b4a6854f0ff4
Sergei Chipiga <chipiga86@gmail.com>
2018-05-03 07:26:18 +0300

    Allure report fixes

--- tests/e2e/testTimerSteps.js
+++ tests/e2e/testTimerSteps.js
@@ -1,32 +1,35 @@
 "use strict";
 
-test("It should sleep 1 sec", () => {
-    chunk(async () => {
-        await SS.pause(1, "sleep 1 sec");
-    });
-});
+suite("Timer steps", () => {
 
-test("It should pass timer check", () => {
-    chunk(async () => {
-        await SS.startTimer();
-        await SS.pause(1, "sleep");
-        await SS.checkTimer({ "to be above": 1 });
+    test("It should sleep 1 sec", () => {
+        chunk(async () => {
+            await SS.pause(1, "sleep 1 sec");
+        });
     });
-});
 
-test("It should fail timer check", () => {
-    chunk(async () => {
-        await SS.startTimer();
-        await SS.pause(1, "sleep");
-        await SS.checkTimer({ "to be below": 1 });
+    test("It should pass timer check", () => {
+        chunk(async () => {
+            await SS.startTimer();
+            await SS.pause(1, "sleep");
+            await SS.checkTimer({ "to be above": 1 });
+        });
     });
-});
 
-test("It should fail because timer isn't started", () => {
-    chunk(async () => {
-        await SS.startTimer();
-        await SS.pause(1, "sleep");
-        await SS.stopTimer();
-        await SS.checkTimer({ "to be equal": 1 });
+    test("It should fail timer check", () => {
+        chunk(async () => {
+            await SS.startTimer();
+            await SS.pause(1, "sleep");
+            await SS.checkTimer({ "to be below": 1 });
+        });
+    });
+
+    test("It should fail because timer isn't started", () => {
+        chunk(async () => {
+            await SS.startTimer();
+            await SS.pause(1, "sleep");
+            await SS.stopTimer();
+            await SS.checkTimer({ "to be equal": 1 });
+        });
     });
 });
