4d2934118752f0e36d1445f9cba1be4bf7d82e9f
Sergei Chipiga <chipiga86@gmail.com>
2018-07-20 07:50:33 +0300

    Add unit tests for base reporter on suite processing

--- tests/unit/testReporterBase.js
+++ tests/unit/testReporterBase.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const GlaceReporter = rewire("../../lib/reporter/base");
-const TestCase = rewire("../../lib/testing").TestCase;
+const testing = rewire("../../lib/testing");
 
 const methods = {};
 const runner = {
@@ -75,7 +75,7 @@ suite("reporter/base", () => {
         });
 
         chunk("tests session is failed if there are failed tests", () => {
-            conf.test.cases.push({ status: TestCase.FAILED });
+            conf.test.cases.push({ status: testing.TestCase.FAILED });
             onEnd();
             expect(conf.session.isPassed).to.be.false;
         });
@@ -87,7 +87,7 @@ suite("reporter/base", () => {
         });
 
         chunk("test session is passed if not failed tests and session errors", () => {
-            conf.test.cases.push({ status: TestCase.PASSED });
+            conf.test.cases.push({ status: testing.TestCase.PASSED });
             GlaceReporter.__set__("sessErrsNum", 0);
             onEnd();
             expect(conf.session.isPassed).to.be.true;
@@ -107,6 +107,107 @@ suite("reporter/base", () => {
             reporters.push({ end: sinon.spy() });
             onEnd();
             expect(reporters[0].end).to.be.calledOnce;
+        });
+    });
+
+    test("on suite", () => {
+        let onSuite, reporters;
+
+        beforeChunk(() => {
+            onSuite = methods["suite"];
+            reporters = [];
+            GlaceReporter.__set__("reporters", reporters);
+        });
+
+        ["suite", "scope", "test"].forEach(methodName => {
+            let mochaSuite;
+
+            beforeChunk(() => {
+                mochaSuite = {
+                    title: new testing.ScopeType("my").setType(methodName),
+                };
+                reporters.push({});
+                reporters[0][methodName] = sinon.spy();
+            });
+
+            chunk(`${methodName} is called if exists`, () => {
+                onSuite(mochaSuite);
+                expect(reporters[0][methodName]).to.be.calledOnce;
+                expect(reporters[0][methodName].args[0][0]).to.be.equal(mochaSuite);
+            });
+        });
+
+        chunk("nothing happends if no reporters exist", () => {
+            let mochaSuite = {
+                title: new testing.ScopeType("my").setType("suite"),
+            };
+            onSuite(mochaSuite);
+        });
+
+        chunk("nothing happends if reporter method doesn't exist", () => {
+            reporters.push({
+                suite: sinon.spy(),
+                scope: sinon.spy(),
+                test: sinon.spy(),
+            });
+            let mochaSuite = {
+                title: new testing.ScopeType("my"),
+            };
+            onSuite(mochaSuite);
+            expect(reporters[0].suite).to.not.be.called;
+            expect(reporters[0].scope).to.not.be.called;
+            expect(reporters[0].test).to.not.be.called;
+        });
+    });
+
+    test("on suite end", () => {
+        let onSuiteEnd, reporters;
+
+        beforeChunk(() => {
+            onSuiteEnd = methods["suite end"];
+            reporters = [];
+            GlaceReporter.__set__("reporters", reporters);
+        });
+
+        ["suite", "scope", "test"].forEach(type => {
+            let mochaSuite,
+                methodName = type + "End";
+
+            beforeChunk(() => {
+                mochaSuite = {
+                    title: new testing.ScopeType("my").setType(type),
+                };
+                reporters.push({});
+                reporters[0][methodName] = sinon.spy();
+            });
+
+            chunk(`${methodName} is called if exists`, () => {
+                onSuiteEnd(mochaSuite);
+                expect(reporters[0][methodName]).to.be.calledOnce;
+                expect(reporters[0][methodName].args[0][0]).to.be.equal(mochaSuite);
+            });
+        });
+
+        chunk("nothing happends if no reporters exist", () => {
+            let mochaSuite = {
+                title: new testing.ScopeType("my").setType("suite"),
+            };
+            onSuiteEnd(mochaSuite);
+        });
+
+        chunk("nothing happends if reporter method doesn't exist", () => {
+            reporters.push({
+                suiteEnd: sinon.spy(),
+                scopeEnd: sinon.spy(),
+                testEnd: sinon.spy(),
+            });
+            let mochaSuite = {
+                title: new testing.ScopeType("my"),
+            };
+            onSuiteEnd(mochaSuite);
+            expect(reporters[0].suiteEnd).to.not.be.called;
+            expect(reporters[0].scopeEnd).to.not.be.called;
+            expect(reporters[0].testEnd).to.not.be.called;
         });
     });
 });
