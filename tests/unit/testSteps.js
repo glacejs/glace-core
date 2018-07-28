79ba9139003a00ab8ca030f7573308b1dc538307
Sergei Chipiga <chipiga86@gmail.com>
2018-07-28 13:23:03 +0300

    Cover steps with unittests

--- tests/unit/testSteps.js
+++ tests/unit/testSteps.js
@@ -1,18 +1,17 @@
 "use strict";
 
-var Steps = require("../../lib").Steps;
+const Steps = rewire("../../lib/steps");
 
-var obj1 = {
+const obj1 = {
     method1: function () {},
 };
 
-var obj2 = {
+const obj2 = {
     method2: function () {},
 };
 
-test("Steps class", () => {
-
-    var steps;
+suite("Steps", () => {
+    let steps;
 
     before(() => {
         steps = new Steps();
@@ -22,44 +21,197 @@ test("Steps class", () => {
         steps.stopTimer();
     });
 
-    chunk(".register() should register steps", () => {
-        Steps.register(obj1, obj2);
-        expect(Steps.prototype.method1).to.be.a("function");
-        expect(Steps.prototype.method2).to.be.a("function");
+    afterChunk(() => {
+        Steps.__reset__();
     });
 
-    chunk(".startTimer() should start timer", () => {
-        steps.startTimer();
-        expect(steps._timer).to.exist.and.be.lte(new Date);
+    test(".register()", () => {
+
+        chunk("should register steps", () => {
+            Steps.register(obj1, obj2);
+            expect(Steps.prototype.method1).to.be.a("function");
+            expect(Steps.prototype.method2).to.be.a("function");
+        });
     });
 
-    chunk(".stopTimer() should stop timer", () => {
-        steps.startTimer();
-        steps.stopTimer();
-        expect(steps._timer).to.not.exist;
+    test(".getInstance() returns object which", () => {
+        let cls, conf, console_;
+
+        beforeChunk(() => {
+            cls = function () {
+                this.prop = 1;
+            };
+            cls.prototype.debug = sinon.spy();
+            cls.prototype.method = sinon.stub().returns(1);
+
+            conf = {
+                session: {
+                    debugOnFail: true,
+                },
+            };
+
+            console_ = {
+                log: sinon.spy(),
+            };
+
+            Steps.__set__("CONF", conf);
+            Steps.__set__("console", console_);
+        });
+
+        chunk("has non-modified properties", () => {
+            expect(Steps.getInstance(cls).prop).to.be.equal(new cls().prop);
+        });
+
+        chunk("has non-modified 'debug' method", () => {
+            expect(Steps.getInstance(cls).debug).to.be.equal(new cls().debug);
+        });
+
+        chunk("has non-modified methods if debug mode is disabled", () => {
+            conf.session.debugOnFail = false;
+            expect(Steps.getInstance(cls).method).to.be.equal(new cls().method);
+        });
+
+        chunk("has wrapped methods if debug mode is enabled", async () => {
+            expect(Steps.getInstance(cls).method).to.not.be.equal(new cls().method);
+            expect(await Steps.getInstance(cls).method()).to.be.equal(new cls().method());
+        });
+
+        chunk("has wrapped methods which launch 'debug' method on exception", async () => {
+            cls.prototype.method.throws(Error("BOOM!"));
+            await expect(Steps.getInstance(cls).method()).to.be.rejectedWith("BOOM!");
+            expect(cls.prototype.debug).to.be.calledOnce;
+            expect(console_.log).to.be.calledOnce;
+            expect(console_.log.args[0][0]).to.include("BOOM!");
+        });
     });
 
-    chunk(".checkTimer() throws error if timer isn't started", () => {
-        expect(steps.checkTimer).to.throw();
+    test(".resetCtx()", () => {
+
+        chunk(() => {
+            steps.ctx = { a: 1 };
+            steps.resetCtx();
+            expect(steps.ctx).to.be.eql({});
+        });
     });
 
-    chunk(".checkTimer() matches condition", () => {
-        steps.startTimer();
-        steps.checkTimer({ lte: 1 });
+    test(".isTestFailed()", () => {
+        let conf;
+
+        beforeChunk(() => {
+            conf = {
+                test: {},
+            };
+
+            Steps.__set__("CONF", conf);
+        });
+
+        chunk("returns undefined if no test case", () => {
+            expect(steps.isTestFailed()).to.be.undefined;
+        });
+
+        chunk("returns false if no test errors", () => {
+            conf.test.curCase = { errors: [] };
+            expect(steps.isTestFailed()).to.be.false;
+        });
+
+        chunk("returns true if there are test errors", () => {
+            conf.test.curCase = { errors: ["BOOM!"] };
+            expect(steps.isTestFailed()).to.be.true;
+        });
     });
 
-    chunk(".checkTimer() throws error if it doesn't match condition", () => {
-        steps.startTimer();
-        expect(() => steps.checkTimer({ gte: 1000 })).to.throw();
+    test(".debug()", () => {
+        let U, setupDebug, conf;
+
+        beforeChunk(() => {
+            U = {
+                debug: sinon.spy(),
+            };
+
+            setupDebug = sinon.stub().returns("help message");
+
+            conf = {
+                session: { debugOnFail: true },
+            };
+
+            Steps.__set__("CONF", conf);
+            Steps.__set__("U", U);
+            Steps.__set__("setupDebug", setupDebug);
+        });
+
+        chunk(async () => {
+            await steps.debug();
+            expect(conf.session.debugOnFail).to.be.true;
+            expect(U.debug).to.be.calledOnce;
+            expect(U.debug.args[0][0]).to.be.equal("help message");
+            expect(setupDebug).to.be.calledOnce;
+        });
     });
 
-    chunk(".pause() sleeps", async () => {
-        steps.startTimer();
-        await steps.pause(0.1, "sleep");
-        steps.checkTimer({ gte: 0.1 });
+    test(".startTimer()", () => {
+
+        chunk("should start timer", () => {
+            steps.startTimer();
+            expect(steps._timer).to.exist.and.be.lte(new Date);
+        });
     });
 
-    chunk(".pause() throws error if message isn't specified", async () => {
-        await expect(steps.pause(0.1)).to.be.rejected;
+    test(".stopTimer()", () => {
+
+        chunk("should stop timer", () => {
+            steps.startTimer();
+            steps.stopTimer();
+            expect(steps._timer).to.not.exist;
+        });
+    });
+
+    test(".checkTimer()", () => {
+
+        chunk("throws error if timer isn't started", () => {
+            expect(steps.checkTimer).to.throw();
+        });
+    
+        chunk("matches condition", () => {
+            steps.startTimer();
+            steps.checkTimer({ lte: 1 });
+        });
+    
+        chunk("throws error if it doesn't match condition", () => {
+            steps.startTimer();
+            expect(() => steps.checkTimer({ gte: 1000 })).to.throw();
+        });
+    });
+
+    test(".pause()", () => {
+
+        chunk("sleeps", async () => {
+            steps.startTimer();
+            await steps.pause(0.1, "sleep");
+            steps.checkTimer({ gte: 0.1 });
+        });
+    
+        chunk("throws error if message isn't specified", async () => {
+            await expect(steps.pause(0.1)).to.be.rejected;
+        });
+    });
+
+    test("setupDebug()", () => {
+        let setupDebug, global_;
+
+        beforeChunk(() => {
+            setupDebug = Steps.__get__("setupDebug");
+
+            global_ = {};
+            Steps.__set__("global", global_);
+
+            Steps.__set__("$", {
+                listSteps: function () { return "list of steps"; },
+            });
+        });
+
+        chunk(() => {
+            expect(setupDebug()).to.include("- search");
+            expect(global_.search()).to.be.equal("list of steps");
+        });
     });
 });
