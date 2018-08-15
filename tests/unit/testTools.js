4ca8a50e61885e80d1db4f207a34b9d101df6963
Sergei Chipiga <chipiga86@gmail.com>
2018-08-15 14:21:13 +0300

    Add unittests for tools listSteps

--- tests/unit/testTools.js
+++ tests/unit/testTools.js
@@ -8,6 +8,56 @@ suite("tools", () => {
         tools.__reset__();
     });
 
+    test(".listSteps()", () => {
+        let getStepNames, getStepsData, learnClassifier, filterSteps;
+
+        beforeChunk(() => {
+            getStepNames = sinon.stub();
+            tools.__set__("getStepNames", getStepNames);
+
+            getStepsData = sinon.stub();
+            tools.__set__("getStepsData", getStepsData);
+
+            learnClassifier = sinon.stub();
+            tools.__set__("learnClassifier", learnClassifier);
+
+            filterSteps = sinon.stub();
+            tools.__set__("filterSteps", filterSteps);
+        });
+
+        chunk("gets cached steps", () => {
+            tools.__set__("stepsCache", ["my-step"]);
+
+            expect(tools.listSteps()).to.be.eql(["my-step"]);
+            expect(getStepNames).to.not.be.called;
+            expect(getStepsData).to.not.be.called;
+            expect(learnClassifier).to.not.be.called;
+            expect(filterSteps).to.not.be.called;
+        });
+
+        chunk("gets steps with cache population", () => {
+            getStepsData.returns(["my-step"]);
+            tools.__set__("stepsCache", null);
+
+            expect(tools.listSteps()).to.be.eql(["my-step"]);
+            expect(tools.__get__("stepsCache")).to.be.eql(["my-step"]);
+            expect(getStepNames).to.be.calledOnce;
+            expect(getStepsData).to.be.calledOnce;
+            expect(learnClassifier).to.be.calledOnce;
+            expect(learnClassifier.args[0][0]).to.be.eql(["my-step"]);
+            expect(filterSteps).to.not.be.called;
+        });
+
+        chunk("gets filtered steps", () => {
+            filterSteps.returns(["my-another-step"]);
+            tools.__set__("stepsCache", ["my-step"]);
+
+            expect(tools.listSteps("another")).to.be.eql(["my-another-step"]);
+            expect(filterSteps).to.be.calledOnce;
+            expect(filterSteps.args[0]).to.be.eql([["my-step"], "another", false]);
+        });
+    });
+
     test(".fakeLoad()", () => {
         let global_, require_;
 
