5b65f45322920fdd3bd6870972300025cf567227
Sergei Chipiga <chipiga86@gmail.com>
2018-08-16 17:39:35 +0300

    Add unittests for classifier

---
+++ tests/unit/testClassifier.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const classifier = rewire("../../lib/classifier");
+
+suite("classifier", () => {
+    let clsf;
+
+    beforeChunk(() => {
+        clsf = classifier();
+    });
+
+    afterChunk(() => {
+        classifier.__reset__();
+    });
+
+    test(".classify()", () => {
+
+        chunk(() => {
+            clsf.learn("I'm happy every day", "happiness");
+            clsf.learn("I'm sad sometimes", "sadness");
+
+            let result = clsf.classify("happy");
+            expect(result[0].label).to.be.equal("happiness");
+            expect(result[0].value).to.be.gte(0.16);
+            expect(result[1].label).to.be.equal("sadness");
+            expect(result[1].value).to.be.lte(0.10);
+
+            result = clsf.classify("sad");
+            expect(result[0].label).to.be.equal("sadness");
+            expect(result[0].value).to.be.gte(0.16);
+            expect(result[1].label).to.be.equal("happiness");
+            expect(result[1].value).to.be.lte(0.10);
+        });
+    });
+});
