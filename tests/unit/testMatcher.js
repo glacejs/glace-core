6c4fcf763bf05d90eb60faee0fac96532db06777
Sergei Chipiga <chipiga86@gmail.com>
2018-08-25 08:50:16 +0300

    Add tests for matcher

--- tests/unit/testMatcher.js
+++ tests/unit/testMatcher.js
@@ -1,27 +1,45 @@
 "use strict";
 
-test("chai matcher .waitFor() is", () => {
-    var now;
+suite("matcher", () => {
 
-    before(() => {
-        now = new Date().getTime();
+    test(".waitFor()", () => {
+        let now;
+
+        before(() => {
+            now = new Date().getTime();
+        });
+
+        chunk("passed", async () => {
+            let i = 0;
+            await expect(() => i++).to.waitFor({ "to be equal": 5 });
+            expect(new Date().getTime() - now).to.be.gte(500).and.below(1100);
+        });
+
+        chunk("passed for async predicate", async () => {
+            const p = () => new Promise(resolve => setTimeout(() => resolve(5), 500));
+            await expect(p).to.waitFor({ "to be equal": 5 });
+            expect(new Date().getTime() - now).to.be.gte(500).and.below(1100);
+        });
+
+        chunk("failed", async () => {
+            await expect(expect(() => 0).to.waitFor({ "to be equal": 5 }))
+                .to.be.rejectedWith("expected 0 to equal 5");
+            expect(new Date().getTime() - now).to.be.gte(1000);
+        });
     });
 
-    chunk("passed", async () => {
-        var i = 0;
-        await expect(() => i++).to.waitFor({ "to be equal": 5 });
-        expect(new Date().getTime() - now).to.be.gte(500).and.below(1100);
-    });
+    test(".correspond()", () => {
 
-    chunk("passed for async predicate", async () => {
-        var p = () => new Promise(resolve => setTimeout(() => resolve(5), 500));
-        await expect(p).to.waitFor({ "to be equal": 5 });
-        expect(new Date().getTime() - now).to.be.gte(500).and.below(1100);
-    });
+        chunk("passes custom message", () => {
+            expect(() => expect(1).to.correspond({ "to be equal": 2 }, "wrong values")).to.throw("wrong values");
+        });
 
-    chunk("failed", async () => {
-        await expect(expect(() => 0).to.waitFor({ "to be equal": 5 }))
-            .to.be.rejectedWith("expected 0 to equal 5");
-        expect(new Date().getTime() - now).to.be.gte(1000);
+        chunk("should contain only one key-value pair", () => {
+            expect(() => expect(1).to.correspond({ "to be equal": 1, "to be gte": 0 })).to.throw("only one key-value");
+        });
+
+        chunk("should be string or object only", () => {
+            expect(() => expect(1).to.correspond(1)).to.throw("string or object only");
+        });
     });
 });
